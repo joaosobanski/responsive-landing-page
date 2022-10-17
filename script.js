0ea397bf498eb4147833f8dd957c77f3b859dca2
João Vitor Sobanski <54563534+joaosobanski@users.noreply.github.com>
2022-10-17 13:06:10 -0300

    .

--- script.js
+++ script.js
@@ -19,50 +19,50 @@ AOS.init();
 // ==== LOGO  ==== //
 gsap.from(".logo", {
   opacity: 0,
-  y: -10,
-  delay: 1,
-  duration: 0.5,
+  y: -5,
+  delay: 0.1,
+  duration: 0.1,
 });
 // ==== NAV-MENU ==== //
 gsap.from(".nav_menu_list .nav_menu_item", {
   opacity: 0,
-  y: -10,
-  delay: 1.4,
-  duration: 0.5,
+  y: -5,
+  delay: 0.4,
+  duration: 0.1,
   stagger: 0.3,
 });
 // ==== TOGGLE BTN ==== //
 gsap.from(".toggle_btn", {
   opacity: 0,
-  y: -10,
-  delay: 1.4,
-  duration: 0.5,
+  y: -5,
+  delay: 0.5,
+  duration: 0.1,
 });
 // ==== MAIN HEADING  ==== //
 gsap.from(".main-heading", {
   opacity: 0,
   y: 20,
-  delay: 2.4,
+  delay: 0.4,
   duration: 1,
 });
 // ==== INFO TEXT ==== //
 gsap.from(".info-text", {
   opacity: 0,
   y: 20,
-  delay: 2.8,
+  delay: 0.6,
   duration: 1,
 });
 // ==== CTA BUTTONS ==== //
 gsap.from(".btn_wrapper", {
   opacity: 0,
   y: 20,
-  delay: 2.8,
+  delay: 1.2,
   duration: 1,
 });
 // ==== TEAM IMAGE ==== //
 gsap.from(".team_img_wrapper img", {
   opacity: 0,
-  y: 20,
-  delay: 3,
+  y: 150,
+  delay: 1.2,
   duration: 1,
 });
